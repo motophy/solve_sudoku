188c3d14c19f89cd012972a0b9c321017a3f2742
motophy <jt199255@gmail.com>
2023-06-02 17:33:04 +0900

    add annotation

--- solve_sudoku.py
+++ solve_sudoku.py
@@ -10,19 +10,24 @@ def is_valid(sudoku, row, col, num):
     return True
 
 def solve_sudoku(sudoku, row=0, col=0):
+    # 순서대로 칸 이동하기
     if col == 9:
         if row == 8:
             return True
         row += 1
         col = 0
-
+    # 빈칸이 아닐경우 넘기기(빈칸은 0으로)
     if sudoku[row][col] != 0:
         return solve_sudoku(sudoku, row, col + 1)
     for num in range(1, 10):
+        # 순차적으로 숫자 대입하여 확인하기
         if is_valid(sudoku, row, col, num):
+            # true 반환시 해당 숫자 스도쿠에 입력
             sudoku[row][col] = num
+            # 다음칸으로 넘어가기
             if solve_sudoku(sudoku, row, col + 1):
                 return True
+    # 1~9까지 숫자대입에 실패하였을경우 False 반환하고 빈칸 0을 다시 넣고 이전 칸으로 재귀
     sudoku[row][col] = 0
     return False
 
